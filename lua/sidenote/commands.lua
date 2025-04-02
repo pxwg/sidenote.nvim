09e6f5d2013cd02ed825e3ab33e468f753cad582
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-04-02 21:02:57 +0800

    debug: unexpected empty line in data base

--- lua/sidenote/commands.lua
+++ lua/sidenote/commands.lua
@@ -6,53 +6,7 @@ local upd = require("utils.update")
 local update = require("utils.update")
 local vt = require("utils.vt")
 
---- TODO: be sure to reset all the virtual text when the line has been changed
-vim.api.nvim_create_user_command("SidenoteInsert", function()
-  vim.cmd("SidenoteRestoreAll")
-  local file_path = vim.api.nvim_buf_get_name(0)
-  local db_path = path.get_db_path(file_path)
-  local line = vim.api.nvim_win_get_cursor(0)[1]
-  local col = vim.api.nvim_win_get_cursor(0)[2]
-  local buf = vim.api.nvim_get_current_buf()
-  local id = vt.get_virtual_text_id_at_cursor()
-  db.create_tbl(db_path)
-
-  --- update
-  if upd.is_sidenote_exist(line) == true then
-    local origin_text = db.get_by_id(db_path, id)[1].text
-    ui.input_float({
-      initial_text = origin_text,
-      title = "Update Sidenote",
-      callback = function(text)
-        if text == "" then
-          db.delete_by_id(db_path, id)
-          return
-        end
-        db.update_by_id(db_path, id, { text = text, line = line })
-        vt.remove_virtual_text_from_line(buf, line - 1)
-        vt.add_virtual_line_with_connector(buf, line - 1, col, text, "Comment", id)
-      end,
-    })
-  else
-    local vt_id = tags.generateTimestampTag()
-    ui.input_float({
-      title = "Insert Sidenote",
-      callback = function(text)
-        db.add_entry(db_path, {
-          filepath = file_path,
-          line = vim.api.nvim_win_get_cursor(0)[1],
-          col = vim.api.nvim_win_get_cursor(0)[2],
-          text = text,
-          vt_id = id,
-        })
-        vt.add_virtual_line_with_connector(buf, line - 1, col, text, "Comment", vt_id)
-      end,
-    })
-  end
-end, {})
-
-vim.api.nvim_create_user_command("SidenoteRestoreAll", function()
-  -- update.clean_invalid_sidenotes()
+local function restore_all()
   local filepath = vim.api.nvim_buf_get_name(0)
   local db_path = path.get_db_path(filepath)
   db.create_tbl(db_path)
@@ -68,6 +22,69 @@ vim.api.nvim_create_user_command("SidenoteRestoreAll", function()
       vt.add_virtual_line_with_connector(buf, line - 1, col, sidenote.text, "Comment", id)
     end
   end
+end
+
+vim.api.nvim_create_user_command("SidenoteFoldAll", function()
+  local filepath = vim.api.nvim_buf_get_name(0)
+  local db_path = path.get_db_path(filepath)
+  local buf = vim.api.nvim_get_current_buf()
+  local sidenotes = db.get_all_sidenotes(db_path)
+  for _, sidenote in ipairs(sidenotes) do
+    local line = sidenote.line
+    vt.remove_virtual_text_from_line(buf, line - 1)
+  end
+end, {})
+
+--- TODO: be sure to reset all the virtual text when the line has been changed
+vim.api.nvim_create_user_command("SidenoteInsert", function()
+  restore_all()
+  local file_path = vim.api.nvim_buf_get_name(0)
+  local db_path = path.get_db_path(file_path)
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  local buf = vim.api.nvim_get_current_buf()
+  local id = vt.get_virtual_text_id_at_cursor()
+  db.create_tbl(db_path)
+
+  --- update
+  if upd.is_sidenote_exist(line) == true then
+    local origin_text = db.get_by_id(db_path, id)[1].text or ""
+    ui.input_float({
+      initial_text = origin_text,
+      title = "Update Sidenote",
+      callback = function(text)
+        if text == "" then
+          db.delete_by_id(db_path, id)
+          return
+        end
+        db.update_by_id(db_path, id, { text = text, line = line })
+        vt.remove_virtual_text_from_line(buf, line - 1)
+        if id > 0 then
+          vt.add_virtual_line_with_connector(buf, line - 1, col, text, "Comment", id)
+        end
+      end,
+    })
+  else
+    local vt_id = tags.generateTimestampTag()
+    ui.input_float({
+      title = "Insert Sidenote",
+      callback = function(text)
+        db.add_entry(db_path, {
+          filepath = file_path,
+          line = vim.api.nvim_win_get_cursor(0)[1],
+          col = vim.api.nvim_win_get_cursor(0)[2],
+          text = text,
+          vt_id = vt_id or 1,
+        })
+        vt.add_virtual_line_with_connector(buf, line - 1, col, text, "Comment", vt_id)
+      end,
+    })
+  end
+end, {})
+
+vim.api.nvim_create_user_command("SidenoteRestoreAll", function()
+  -- update.clean_invalid_sidenotes()
+  restore_all()
 end, {})
 
 vim.api.nvim_create_user_command("SidenoteFoldAll", function()
